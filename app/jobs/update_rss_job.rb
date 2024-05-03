017ba21f6d3412895216a854db4dd5a10665045a
ferdi2005 <ferdi.traversa@gmail.com>
2024-05-04 01:59:05 +0300

    Fix tag

--- app/jobs/update_rss_job.rb
+++ app/jobs/update_rss_job.rb
@@ -31,7 +31,7 @@ class UpdateRssJob < ApplicationJob
 
         Chat.all.each do |chat|
           begin
-            bot.api.send_message(chat_id: chat.chat_id, text: "<br>Nuovo messaggio al bar #{external ? "(esterno)</b>" : ""}: <a href='#{url}'>#{title}</a>", parse_mode: :HTML)
+            bot.api.send_message(chat_id: chat.chat_id, text: "<b>Nuovo messaggio al bar #{external ? "(esterno)</b>" : ""}: <a href='#{url}'>#{title}</a>", parse_mode: :HTML)
           rescue => e
             bot.api.send_message(chat_id: ENV["FALLBACK"].to_i, text: "Errore #{e} nell'invio del massagigo al bar <a href='#{url}'>#{title}</a> a #{chat.chat_id} - #{chat.username}", parse_mode: :HTML)
           end
